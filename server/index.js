074760f25f1f8c97a259117a34e32adcaf6e6c6d
cipher <karthikasundaram42@gmail.com>
2026-02-20 21:31:25 +0530

    corrected user creation

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js'
 import { clerkWebhooks } from './controllers/webhooks.js'
+import educatorRouter from './routes/educatorRoutes.js'
+import { clerkMiddleware } from '@clerk/express'
 
 //Initialize Express
 const app = express()
@@ -14,10 +16,12 @@ await connectDB()
 
 //middleware
 app.use(cors())
+app.use(clerkMiddleware())
 
 //Routes
 app.get('/',(req, res)=> res.send("API Working"))
 app.post('/clerk', express.json(), clerkWebhooks)
+app.use('/api/educator',express.json(), educatorRouter)
 
 //port 
 const PORT = process.env.PORT || 5000
