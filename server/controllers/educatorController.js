330bd21f60260dc61b7101fa890e85cc02c63eda
cipher <karthikasundaram42@gmail.com>
2026-02-28 10:31:07 +0530

    commit changes App

--- server/controllers/educatorController.js
+++ server/controllers/educatorController.js
@@ -4,9 +4,9 @@ import { clerkClient } from '@clerk/express'
 
 // update role to educator
 
-export const updateRoleToEducator = async (req, res)=>{
+export const updateRoleToEducator = async (req,res)=>{
     try{
-        const  userId  = req.auth.userId
+        const  userId  = req.auth().userId
         await clerkClient.users.updateUserMetadata(userId,{
             publicMetadata:{
                 role : 'educator',
